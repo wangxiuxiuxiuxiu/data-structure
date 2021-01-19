fe4e6678be569cded42e95a3ab7b68e8a9654645
yzs <1064118784@qq.com>
2021-01-19 15:50:47 +0800

    二分查找变体新增

--- src/sort/BinarySearch.java
+++ src/sort/BinarySearch.java
@@ -8,8 +8,10 @@ public class BinarySearch {
         int arr[] = {6,9,15,25,68,99,120,120,120,560,666,872};
         //int value = binary(arr,arr.length,560);
        // int value = binarySearch(arr,0,arr.length-1,560);
-       // int value = getFirstElement(arr,0,arr.length-1,120);
-        int value = getLastElement(arr,0,arr.length-1,120);
+      //  int value = getFirstElement(arr,0,arr.length-1,120);
+       // int value = getLastElement(arr,0,arr.length-1,120);
+        int value = getFirstBigElement(arr,0,arr.length-1,60);
+        //int value = getLastLessElement(arr,0,arr.length-1,60);
         System.out.println(value);
     }
 
@@ -51,7 +53,7 @@ public class BinarySearch {
 
     public static  int getFirstElement(int arr[],int low,int hight,int value){
 
-        while (low<hight){
+        while (low<=hight){
             int mid = low + ((hight-low)>>1);
             if (arr[mid]>value){
                 hight = mid-1;
@@ -61,7 +63,7 @@ public class BinarySearch {
                 //如果中间值等于给定值，判断mid 是否是数组第一个值，或mid 的前一个值是否等于给定值
                 if(mid==0||arr[mid-1]!=value){
                     System.out.println("数组角标："+mid);
-                    return value;
+                    return arr[mid];
                 }else {
                     hight = mid-1;
                 }
@@ -70,10 +72,10 @@ public class BinarySearch {
 
         return -1;
     }
-    //变体一：查找最后一个值等于给定值的元素
+    //变体二：查找最后一个值等于给定值的元素
 
     public static  int getLastElement(int arr[],int low,int hight,int value){
-        while (low<hight){
+        while (low<=hight){
             int mid = low + ((hight-low)>>1);
             if (arr[mid]>value){
                 hight = mid-1;
@@ -83,7 +85,48 @@ public class BinarySearch {
                 //如果中间值等于给定值，判断mid 是否是数组最后一个值，或mid 的后一个值是否等于给定值
                 if(mid==arr.length-1||arr[mid+1]!=value){
                     System.out.println("数组角标："+mid);
-                    return value;
+                    return arr[mid];
+                }else {
+                    low = mid+1;
+                }
+            }
+        }
+
+        return -1;
+    }
+    //变体三：查找第一个大于等于给定值的元素
+
+    public static  int getFirstBigElement(int arr[],int low,int hight,int value){
+        while (low<=hight){
+            int mid = low + ((hight-low)>>1);
+
+            if ((arr[mid]<value)){
+                low = mid+1;
+            }else{
+               if (mid==0||arr[mid-1]<value){
+                   System.out.println("数组角标："+mid);
+                   return arr[mid];
+               }else {
+                   hight = mid-1;
+               }
+            }
+        }
+
+        return -1;
+    }
+
+    //变体四：查找最后一个小于等于给定值的元素
+
+    public static  int getLastLessElement(int arr[],int low,int hight,int value){
+
+        while (low<=hight){
+            int mid = low + ((hight-low)>>1);
+            if ((arr[mid]>value)){
+                hight = mid-1;
+            }else{
+                if (mid==arr.length-1||arr[mid+1]>value){
+                    System.out.println("数组角标："+mid);
+                    return arr[mid];
                 }else {
                     low = mid+1;
                 }
